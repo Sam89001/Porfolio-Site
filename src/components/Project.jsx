950ccc4bdeccd08d7432efc4d36bf4bd4d2ac8bc
Samuel Cole <samscreativespace@gmail.com>
2024-06-11 18:34:04 +0100

    efficiency changes

---
+++ src/components/Project.jsx
@@ -0,0 +1,68 @@
+import '../css/site.css'
+import '../css/index.css'
+import '../css/projectsPage.css'
+
+import GreenCircle from '../assets/Green Circle.png'
+
+
+function Project({ ProjectTitle, ProjectImage, Logo, BackgroundColour }) {
+  return (
+    <div className='d-flex justify-content-center projects-page-component-container standard-bottom-padding' style={{backgroundColor: BackgroundColour, paddingBottom: '2em'}}>
+      <div className='component-content-container projects-page-content ' style={{backgroundColor: BackgroundColour}}>
+            <div className='row h-100'>
+
+              {/* Hideable Title*/}
+              <div className='d-flex flex-row justify-content-between' style={{width: '100%'}}>
+
+                <h1 className='projects-page-project-title projects-page-hideable-content'>{ProjectTitle}</h1>
+
+                <div className='d-flex projects-page-social-media-image-container '>
+                  <div className='projects-page-social-media-image projects-page-hideable-content'>
+                    <img className='img-fluid' src={Logo} alt='Logo'/>
+                  </div>
+
+                  <div className='projects-page-social-media-image projects-page-hideable-content'>
+                    <img className='img-fluid' src={Logo} alt='Logo'/>
+                  </div>
+                </div>
+
+              </div>
+
+              {/* Image */}
+              <div className='col-lg-6 col-md-12 col-sm-12'> 
+                <div className='d-flex' > 
+                  <img className='img-fluid' src={ProjectImage}></img>
+                </div>
+              </div>    
+
+              {/* Text */}
+              <div className='col-lg-6 col-md-12 col-sm-12 d-flex flex-column'> 
+                <h2 className='projects-page-small-project-title'>{ProjectTitle}</h2>
+
+                <p className='project-page-project-paragraph standard-bottom-padding'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+                <div className='projects-page-sub-header' style={{marginTop: 'auto'}}>
+                  <h3>Tech Stack</h3>
+                </div>
+
+                <div className='d-flex flex-row projects-page-tech-stack-images-container'>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+                  <img className='img-fluid projects-page-tech-stack-images' src={GreenCircle} alt='Logo'/>
+
+                </div>
+
+              </div>   
+
+            </div>   
+      </div>
+    </div>
+    )
+}
+
+export default Project
