e6d4f5691dc738c997c42bfb628dce6dd973e946
Samuel Cole <samscreativespace@gmail.com>
2024-06-12 11:55:37 +0100

    started working on contact page

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -4,8 +4,31 @@ import '../css/contactPage.css'
 
 function ContactPage() {
   return (
-    <div>
-        
+    <div className='d-flex justify-content-center contact-page-component-container'>
+      <div className='component-content-container contact-page-content'>
+        <div className='row h-100'>
+
+          <div className='col-12'>
+            <h1 className='main-header standard-bottom-padding'>Lets Connect!</h1>
+          </div>
+
+          {/* Text */}
+
+          <div className='col-lg-6 col-md-12 col-sm-12 row'>
+
+            {/* Individual columns for each item so they can collaps at medium screen sizes
+             Likely col-12 for normal screens and col-4 for medium screen sizes and col-12 for small screen-sizes*/}
+
+          </div>
+
+          {/* Form */}
+
+          <div className='col-lg-6 col-md-12 col-sm-12' >
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
