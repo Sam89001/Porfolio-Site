be16634d8c3dec635e1b71741503ac1ebc92d676
Samuel Cole <samscreativespace@gmail.com>
2024-06-19 17:55:52 +0100

    popup menu is working but not designed or coded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,64 @@
-import '../css/navbar.css'
-
-import Logo from '../assets/SC.png'
-import HamburgerMenu from '../assets/Hamburger Menu.webp'
-
-import PopoutNavbar from './PopoutNavbar'
+import React, { useState } from 'react';
+import '../css/navbar.css';
+import Logo from '../assets/SC.png';
+import HamburgerMenu from '../assets/Hamburger Menu.webp';
+import PopoutNavbar from './PopoutNavbar';
 
 function Header() {
-return(
-<div className='d-flex justify-content-center align-items-center w-100 navbar-container' >
-<div className='d-flex justify-content-center align-items-center w-100 h-100 navbar-content'>
+  const [isPopoutOpen, setIsPopoutOpen] = useState(false);
 
-    <div className='d-flex w-50 h-100 justify-content-start align-items-center'>
-      <img className='img-fluid navbar-logo' src={Logo} alt='Logo' style={{height: '80%'}}/>
-      <img className='img-fluid navbar-hamburger-menu' src={HamburgerMenu} alt='Menu' style={{height: '80%'}}/>
+  const togglePopout = () => {
+    setIsPopoutOpen(!isPopoutOpen);
+  };
 
-      <nav className='w-100 navbar-links' >
-        <ul className="nav">
-          <li className="navbar-item">
-            <a className="navbar-link" href="#section1">Home</a>
-          </li>
-          <li className="navbar-item">
-            <a className="navbar-link" href="#section2">About</a>
-          </li>
-          <li className="navbar-item">
-            <a className="navbar-link" href="#section3">Projects</a>
-          </li>
-          <li className="navbar-item">
-            <a className="navbar-link" href="#section4">Contact Me</a>
-          </li>
-        </ul>
-      </nav>
+  return (
+    <div className='d-flex justify-content-center align-items-center w-100 navbar-container'>
+      <div className='d-flex justify-content-center align-items-center w-100 h-100 navbar-content'>
+        <div className='d-flex w-50 h-100 justify-content-start align-items-center'>
+          <img className='img-fluid navbar-logo' src={Logo} alt='Logo' style={{ height: '80%' }} />
+          <img
+            className='img-fluid navbar-hamburger-menu'
+            src={HamburgerMenu}
+            alt='Menu'
+            style={{ height: '80%' }}
+            onClick={togglePopout} 
+          />
 
+          <nav className='w-100 navbar-links'>
+            <ul className='nav'>
+              <li className='navbar-item'>
+                <a className='navbar-link' href='#section1'>
+                  Home
+                </a>
+              </li>
+              <li className='navbar-item'>
+                <a className='navbar-link' href='#section2'>
+                  About
+                </a>
+              </li>
+              <li className='navbar-item'>
+                <a className='navbar-link' href='#section3'>
+                  Projects
+                </a>
+              </li>
+              <li className='navbar-item'>
+                <a className='navbar-link' href='#section4'>
+                  Contact Me
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className='d-flex w-50 justify-content-end align-items-center'>
+          <button className='navbar-button black-white-button-styling'>Download Resume</button>
+        </div>
+      </div>
+
+      
+      <PopoutNavbar isVisible={isPopoutOpen} onClose={togglePopout} />
     </div>
-
-    <div className='d-flex w-50 justify-content-end align-items-center'>
-      <button className='navbar-button black-white-button-styling'>Download Resume</button>
-    </div>
-
-  </div>
-</div>
-)
+  );
 }
 
-export default Header
+export default Header;
--- src/components/PopoutNavbar.jsx
+++ src/components/PopoutNavbar.jsx
@@ -4,21 +4,19 @@ import Logo from '../assets/SC.png';
 
 function PopoutNavbar({ isVisible, onClose }) {
   return (
-    <div className={`d-flex w-75 h-100 flex-column popout-navbar`}>
-      
+    <div className={`d-flex w-75 h-100 flex-column popout-navbar ${isVisible ? 'show' : 'hide'}`}>
       {/* Top Section */}
-      <div className='d-flex w-100 justify-content-between' style={{backgroundColor: 'green', height: '15%'}}>
-        <div className='d-flex justify-content-center align-items-center' style={{backgroundColor: 'purple', width: '30%'}}>
+      <div className='d-flex w-100 justify-content-between' style={{ backgroundColor: 'green', height: '15%' }}>
+        <div className='d-flex justify-content-center align-items-center' style={{ backgroundColor: 'purple', width: '30%' }}>
           <button className='h-100 w-100 popout-close-button' onClick={onClose}></button>
         </div>
-
-        <div className='h-100 d-flex' style={{backgroundColor: 'purple', width: '30%'}}>
-          <img className='img-fluid' src={Logo} style={{padding: '30% 20% 30% 20%'}} alt='Logo' />
+        <div className='h-100 d-flex' style={{ backgroundColor: 'purple', width: '30%' }}>
+          <img className='img-fluid' src={Logo} style={{ padding: '30% 20% 30% 20%' }} alt='Logo' />
         </div>
       </div>
 
       {/* Middle Section */}
-      <div className='d-flex w-100 h-100 align-items-center' style={{backgroundColor: 'blue', paddingBottom: '80%'}}>
+      <div className='d-flex w-100 h-100 align-items-center' style={{ backgroundColor: 'blue', paddingBottom: '80%' }}>
         <ul>
           <li>
             <a className='popout-link'>
@@ -44,9 +42,10 @@ function PopoutNavbar({ isVisible, onClose }) {
       </div>
 
       {/* Bottom Section */}
-      <div className='d-flex w-100 align-items-center' style={{backgroundColor: 'yellow', height: '15%', marginTop: 'auto'}}></div>
+      <div className='d-flex w-100 align-items-center' style={{ backgroundColor: 'yellow', height: '15%', marginTop: 'auto' }}></div>
     </div>
   );
 }
 
 export default PopoutNavbar;
+
